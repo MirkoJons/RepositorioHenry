9822df57cf443eb5ac76010072c628bc9932544b
mirkojons <mirkojons@gmail.com>
2023-03-07 03:15:45 -0300

    V-m03-var

---
+++ M03-JS examples/Variables.js
@@ -0,0 +1,30 @@
+//Ejemplos de nomenclatura de las variables de JavaScript
+
+//String
+
+
+var bootcamp = 'Henry';
+
+//Numero
+
+var numeroEntero = '17';
+
+var numeroNegativo = '-4';
+
+var numeroDecimal = '11.75'; 
+ 
+//Booleano
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//Null
+
+var sinNada = null;
