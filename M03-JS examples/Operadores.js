0d229f5cd656f20c4dac4c97a87d296e4ef3d528
mirkojons <mirkojons@gmail.com>
2023-03-07 05:12:34 -0300

    v3.1

---
+++ M03-JS examples/Operadores.js
@@ -0,0 +1,12 @@
+//Ejemplos de operadores
+
+console.log(3+5);
+
+console.log(14-7);
+
+console.log(5 % 4)
+
+console.log('HOLA '+ 'CAMILO');
+var nombre = 'Camilo' ;
+var saludo = 'hola ' + nombre;
+console.log(saludo);
