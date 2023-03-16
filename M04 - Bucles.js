1f1c3d44c16deabd6d01cfdee208f99feeea6937
mirkojons <mirkojons@gmail.com>
2023-03-16 03:30:56 -0300

    m04 finished

---
+++ M04 - Bucles.js
@@ -0,0 +1,16 @@
+// Vamos a declarar una variable suma = 0, para despues ir sumandole +1 de manera iterativa.
+
+var suma = 0;
+// suma = suma + 1;
+// suma = suma + 2;
+// suma = suma + 3;
+// suma = suma + 4;
+// console.log(suma)
+
+// Cuando no sabemos el nro. de iteraciones a realizar, vamos a utilizar un bucle 'while', de la siguiente manera:
+
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+}
+
